481719f76f59a54ddbde4718c3fd8be0de669c8a
Habib MD Saifullah <xavierjames701@gmail.com>
2026-09-25 15:40:34 +0600

    chore: add PM2 ecosystem config for VPS deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ipdokan-server',
+      script: 'server/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5001,
+      },
+    },
+    {
+      name: 'ipdokan-bot',
+      script: 'bot/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
